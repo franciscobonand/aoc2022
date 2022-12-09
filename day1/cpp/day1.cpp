869b5c8f4686b3baf6e854967e453abc784f0362
Matheus Bonavite <matheusbonavite@gmail.com>
2022-12-08 21:26:06 -0300

    chore: more pornographic c++ for part 2 of day 1 aoc2022

--- day1/cpp/day1.cpp
+++ day1/cpp/day1.cpp
@@ -9,14 +9,16 @@ int main()
 
     std::string line;
     std::ifstream input_file("calories.txt");
-    std::tuple<int, int> elfs{0, 0};
+    std::tuple<int, int, int> three_most_profitable_elfs{0, 0, 0};
+    std::tuple<int, std::tuple<int, int, int> &> elfs{0, three_most_profitable_elfs};
 
-    bool compute_buffer = [&]()
+    static int compute_three_most_profitable_elves = [&]()
     {
         while (std::getline(input_file, line))
         {
             std::string_view view_line{line};
-            auto &[current_elf, most_profitable_elf] = elfs;
+            auto &[current_elf, most_profitable_elves] = elfs;
+            auto &[most_profitable_elf, second_most_profitable_elf, third_most_profitable_elf] = most_profitable_elves;
 
             if (*view_line.begin() != '\0' && *view_line.begin() != '\n')
             {
@@ -26,14 +28,25 @@ int main()
             {
                 if (current_elf > most_profitable_elf)
                 {
+                    third_most_profitable_elf = second_most_profitable_elf;
+                    second_most_profitable_elf = most_profitable_elf;
                     most_profitable_elf = current_elf;
+                }
+                else if (current_elf > second_most_profitable_elf)
+                {
+                    third_most_profitable_elf = second_most_profitable_elf;
+                    second_most_profitable_elf = current_elf;
+                }
+                else if (current_elf > third_most_profitable_elf)
+                {
+                    third_most_profitable_elf = current_elf;
                 }
                 current_elf = 0;
             }
         }
-        return true;
+        return std::get<0>(three_most_profitable_elfs) + std::get<1>(three_most_profitable_elfs) + std::get<2>(three_most_profitable_elfs);
     }();
 
-    std::cout << "The value of old_buffer is : " << std::get<1>(elfs) << std::endl;
+    std::cout << "The value of compute_three_most_profitable_elves is : " << compute_three_most_profitable_elves << std::endl;
     return 0;
 }
