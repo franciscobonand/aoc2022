06089bf4ff347f72c8c3d87e02441be9b33fa777
Thiago Mohallem <thiagomohallem1@gmail.com>
2022-12-09 00:39:55 -0300

    fix: reading file sync to avoid EOF bug

--- day1/dart/main.dart
+++ day1/dart/main.dart
@@ -1,26 +1,30 @@
-import 'dart:convert';
 import 'dart:io';
+
+bool _isLastLine(int lineCounter, int linesLength) =>
+    lineCounter == linesLength - 1;
 
 Future<void> main(List<String> args) async {
   int auxCalories = 0;
 
   List<int> somasCalorias = [];
+  File file = new File('input.txt');
 
-  await new File('input.txt')
-      .openRead()
-      .transform(utf8.decoder)
-      .transform(new LineSplitter())
-      .forEach(
-    (l) async {
-      if (l.isEmpty) {
+  List<String> lines = file.readAsLinesSync();
+
+  for (int lineCounter = 0; lineCounter < lines.length; lineCounter++) {
+    final line = lines[lineCounter];
+
+    if (line.isEmpty) {
+      somasCalorias.add(auxCalories);
+      auxCalories = 0;
+    } else {
+      auxCalories += int.parse(line);
+      if (_isLastLine(lineCounter, lines.length)) {
         somasCalorias.add(auxCalories);
-
-        auxCalories = 0;
-      } else {
-        auxCalories += int.parse(l);
       }
-    },
-  );
+    }
+  }
+
   somasCalorias.sort((a, b) => b.compareTo(a));
 
   print('Parte 1 - Mais calorias: ${somasCalorias[0]}');
