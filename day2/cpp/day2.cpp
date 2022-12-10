975e260864a4f12553ec286864a9d73c6df03590
Matheus Bonavite <matheusbonavite@gmail.com>
2022-12-10 12:09:54 -0300

    chore2: Another day another dollar, this code is even nastier than the first

--- day2/cpp/day2.cpp
+++ day2/cpp/day2.cpp
@@ -13,6 +13,11 @@
 #define Scissors 'C'
 #define ResponseScissors 'Z'
 
+// Turns out the elf was Benito Mussolini and could not explain shit
+#define ImALoser 'X'
+#define ImTight 'Y'
+#define ImSomethingSomething 'Z'
+
 // Y'all got moves and they mean somethin
 enum class MoveValues
 {
@@ -31,48 +36,47 @@ enum class Outcomes
 
 // Don't mantain the order and you should perish
 std::unordered_map<char, MoveValues> my_response = {
-    {ResponseRock, MoveValues::rock},
-    {ResponsePaper, MoveValues::paper},
-    {ResponseScissors, MoveValues::scissors}};
+    {Rock, MoveValues::rock},
+    {Paper, MoveValues::paper},
+    {Scissors, MoveValues::scissors}};
 
 // This will be the possible ways when the elf plays something I will either win, tie or die (I mean lose)
 std::unordered_map<char, std::tuple<char, char, char>> elf_crusher = {
-    {Rock, {ResponsePaper, ResponseRock, ResponseScissors}},
-    {Paper, {ResponseScissors, ResponsePaper, ResponseRock}},
-    {Scissors, {ResponseRock, ResponseScissors, ResponsePaper}}};
+    {Rock, {Paper, Rock, Scissors}},
+    {Paper, {Scissors, Paper, Rock}},
+    {Scissors, {Rock, Scissors, Paper}}};
 
 int main()
 {
     int total_score = 0;
     std::ifstream my_file("input1.txt");
     std::string line;
-    char elf_move, my_move;
+    char elf_move, my_destiny;
 
     // This is as close to fascism as it can gets
     static auto line_splitter = [&]()
     {
         std::string_view line_view{line};
         elf_move = *line_view.begin();
-        my_move = *(line_view.begin() + 2);
+        my_destiny = *(line_view.begin() + 2);
     };
 
     while (getline(my_file, line))
     {
         line_splitter();
         auto &[winning_char, we_tie_it_up_tight, life_is_rough] = elf_crusher[elf_move];
-        int my_move_has_values = static_cast<int>(my_response[my_move]);
 
-        if (my_move == winning_char)
+        if (my_destiny == ImSomethingSomething)
         {
-            total_score += static_cast<int>(Outcomes::won) + my_move_has_values;
+            total_score += static_cast<int>(Outcomes::won) + static_cast<int>(my_response[winning_char]);
         }
-        else if (my_move == we_tie_it_up_tight)
+        else if (my_destiny == ImTight)
         {
-            total_score += static_cast<int>(Outcomes::draw) + my_move_has_values;
+            total_score += static_cast<int>(Outcomes::draw) + static_cast<int>(my_response[we_tie_it_up_tight]);
         }
         else
         {
-            total_score += static_cast<int>(Outcomes::lost) + my_move_has_values;
+            total_score += static_cast<int>(Outcomes::lost) + static_cast<int>(my_response[life_is_rough]);
         }
     }
     std::cout << "Total score value is: " << total_score << std::endl;
