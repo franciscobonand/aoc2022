9839bbcbc335c40da1ebf3ac9c507ed3d06167d9
Francisco Andrade <francisco@inspr.com>
2022-12-15 20:07:50 -0300

    chore: day 8 part 2

--- day8/go/main.go
+++ day8/go/main.go
@@ -17,21 +17,31 @@ var getIdx = map[bool]func(int, int) string{
 	},
 }
 
+type TreeInfo struct {
+	Seen bool
+	TTL  int
+	TTR  int
+	TTT  int
+	TTB  int
+}
+
 // TODO: make this shit run in parallel
 func main() {
 	input := getInput("input.txt")
-	seen := make(map[string]bool, len(input)*len(input[0]))
-	fillBorders(seen, len(input))
-	checkRows(input, seen)
-	checkColumns(input, seen)
-	fmt.Println(countVisible(seen, len(input)))
+	trees := map[string]*TreeInfo{}
+	initializeTrees(trees, len(input))
+	fillBorders(trees, len(input))
+	checkRows(input, trees)
+	checkColumns(input, trees)
+	fmt.Println(countVisible(trees, len(input)))
+	fmt.Println(getHighestScenicScore(trees, len(input)))
 }
 
-func countVisible(seen map[string]bool, n int) int {
+func countVisible(trees map[string]*TreeInfo, n int) int {
 	count := 0
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			if seen[fmt.Sprintf("x%dy%d", i, j)] {
+			if trees[fmt.Sprintf("x%dy%d", i, j)].Seen {
 				count++
 			}
 		}
@@ -39,56 +49,98 @@ func countVisible(seen map[string]bool, n int) int {
 	return count
 }
 
-func checkRows(seqs []string, seen map[string]bool) {
+func getHighestScenicScore(trees map[string]*TreeInfo, n int) int {
+	max := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			tree := trees[fmt.Sprintf("x%dy%d", i, j)]
+			score := tree.TTL * tree.TTR * tree.TTT * tree.TTB
+			if score > max {
+				max = score
+			}
+		}
+	}
+	return max
+}
+
+func checkRows(seqs []string, trees map[string]*TreeInfo) {
 	for i, seq := range seqs {
-		checkLeftView(i, seq, false, seen)
-		checkRightView(i, seq, false, seen)
+		checkLeftView(i, seq, false, trees)
+		checkRightView(i, seq, false, trees)
 	}
 }
 
 // checkColumns checks transposed matrix (columns)
-func checkColumns(seqs []string, seen map[string]bool) {
+func checkColumns(seqs []string, trees map[string]*TreeInfo) {
 	str := ""
 	for i := 0; i < len(seqs); i++ {
 		for _, seq := range seqs {
 			str = str + string(seq[i])
 		}
-		checkLeftView(i, str, true, seen)
-		checkRightView(i, str, true, seen)
+		checkLeftView(i, str, true, trees)
+		checkRightView(i, str, true, trees)
 		str = ""
 	}
 }
 
-func checkLeftView(currIdx int, seq string, isCol bool, seen map[string]bool) {
+func checkLeftView(currIdx int, seq string, isCol bool, trees map[string]*TreeInfo) {
 	highest := ""
 	fIdx := getIdx[isCol]
 	for i := 0; i < len(seq); i++ {
 		idx := fIdx(currIdx, i)
 		if string(seq[i]) > highest {
 			highest = string(seq[i])
-			seen[idx] = true
+			trees[idx].Seen = true
+		}
+		for j := i - 1; j >= 0; j-- {
+			if isCol {
+				trees[idx].TTT++
+			} else {
+				trees[idx].TTL++
+			}
+			if string(seq[i]) <= string(seq[j]) {
+				break
+			}
 		}
 	}
 }
 
-func checkRightView(currIdx int, seq string, isCol bool, seen map[string]bool) {
+func checkRightView(currIdx int, seq string, isCol bool, trees map[string]*TreeInfo) {
 	highest := ""
 	fIdx := getIdx[isCol]
 	for i := len(seq) - 1; i >= 0; i-- {
 		idx := fIdx(currIdx, i)
 		if string(seq[i]) > highest {
 			highest = string(seq[i])
-			seen[idx] = true
+			trees[idx].Seen = true
+		}
+		for j := i + 1; j < len(seq); j++ {
+			if isCol {
+				trees[idx].TTB++
+			} else {
+				trees[idx].TTR++
+			}
+			if string(seq[i]) <= string(seq[j]) {
+				break
+			}
 		}
 	}
 }
 
-func fillBorders(seen map[string]bool, n int) {
+func fillBorders(trees map[string]*TreeInfo, n int) {
 	for i := 0; i < n; i++ {
-		seen[fmt.Sprintf("x%dy%d", i, 0)] = true
-		seen[fmt.Sprintf("x%dy%d", i, n-1)] = true
-		seen[fmt.Sprintf("x%dy%d", 0, i)] = true
-		seen[fmt.Sprintf("x%dy%d", n-1, i)] = true
+		trees[fmt.Sprintf("x%dy%d", i, 0)].Seen = true
+		trees[fmt.Sprintf("x%dy%d", i, n-1)].Seen = true
+		trees[fmt.Sprintf("x%dy%d", 0, i)].Seen = true
+		trees[fmt.Sprintf("x%dy%d", n-1, i)].Seen = true
+	}
+}
+
+func initializeTrees(trees map[string]*TreeInfo, n int) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			trees[fmt.Sprintf("x%dy%d", i, j)] = &TreeInfo{}
+		}
 	}
 }
 
